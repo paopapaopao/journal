bd3c746f2655aaeaf58d1c539ed12fd64c0d6efc
paopapaopao <paolo.casugay@gmail.com>
2021-07-30 16:41:15 +0800

    Added: update method for CategoriesController

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,9 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def update
+  end
+
   private
     def category_params
       params.require(:category).permit(:name)
