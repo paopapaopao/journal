ac318ca5fe7696c799dfaba04a07dc5992db0844
paopapaopao <paolo.casugay@gmail.com>
2021-08-09 17:53:38 +0800

    Update: category model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,42 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  subject do
-    described_class.new
-  end
+  subject { described_class.new }
 
   let :existing_category do
-    described_class.create(name: 'Not a unique Name')
+    described_class.create(
+      title: 'not a unique title',
+      description: 'a' * 50
+    )
   end
 
-  context 'Name is not present' do
-    context "Name is nil" do
-      it 'does not validate' do
-        subject.name = nil
+  let(:category_on_tasks) { Category.reflect_on_association(:tasks).macro }
+
+  context 'When title is not present' do
+    context 'It is nil' do
+      it do
+        subject.title = nil
         expect(subject).to_not be_valid
       end
     end
 
-    context 'Name is an empty string' do
-      it 'does not validate' do
-        subject.name = ''
+    context 'It is an empty string' do
+      it do
+        subject.title = ''
         expect(subject).to_not be_valid
       end
     end
   end
 
-  context 'Name is not unique' do
-    it 'does not validate' do
+  context 'When title is not unique' do
+    it do
       existing_category
-      subject.name = 'Not a unique Name'
+      subject.title = 'not a unique title'
       expect(subject).to_not be_valid
     end
   end
 
-  context 'Name is unique' do
-    it 'does validate' do
-      subject.name = 'Unique Name'
+  context 'When title is unique' do
+    it do
+      subject.title = 'unique title'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'When description is not present' do
+    context 'It is nil' do
+      it do
+        subject.description = nil
+        expect(subject).to_not be_valid
+      end
+    end
+
+    context 'It is an empty string' do
+      it do
+        subject.description = ''
+        expect(subject).to_not be_valid
+      end
+    end
+  end
+
+  context 'When description is shorter than minimum' do
+    it do
+      subject.description = 'a' * 9
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'When description is longer than maximum' do
+    it do
+      subject.description = 'a' * 101
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'When description is between minimum and maximum' do
+    it do
+      subject.description = 'a' * 50
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'When title and description are valid' do
+    it do
+      subject.title = 'unique title'
+      subject.description = 'a' * 50
       expect(subject).to be_valid
     end
+  end
+
+  context 'Has many tasks' do
+    it { expect(category_on_tasks).to eq :has_many }
   end
 end
