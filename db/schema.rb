0deee39261f7137e7c08bd9c15dcf2365ab8b2d9
paopapaopao <paolo.casugay@gmail.com>
2021-08-09 18:17:15 +0800

    Update: create categories migration file and schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 2021_07_29_021005) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
