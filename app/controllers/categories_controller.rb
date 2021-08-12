69ed4c5356db2e6adf2c9f1aeb44c69dcd5ee492
paopapaopao <paolo.casugay@gmail.com>
2021-08-12 08:34:12 +0800

    Update: categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -35,10 +35,8 @@ class CategoriesController < ApplicationController
 
   def destroy
     @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
-    end
+
+    redirect_to categories_path
   end
 
   private
