c3b36a59a4f9c4d39d88876aba8e47a4f0dab48b
paopapaopao <paolo.casugay@gmail.com>
2021-08-12 11:39:31 +0800

    Update: tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def create
+    @category = Category.find(params[:category_id])
+    @task = @category.tasks.create(task_params)
+    redirect_to category_path(@category)
+  end
+
+  def destroy
+    @category = Category.find(params[:category_id])
+    @task = @category.tasks.find(params[:id])
+    @task = destroy
+    redirect_to category_path(category)
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:description)
+    end
 end
