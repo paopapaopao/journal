a640a82c1d677391c105c8efcc2579917564f5da
paopapaopao <paolo.casugay@gmail.com>
2021-08-18 11:48:45 +0800

    Update: description validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates :description,
-    presence: true, length: { minimum: 10, maximum: 100 }
+    presence: true, length: { in: 10..100 }
 
   belongs_to :category
 end
