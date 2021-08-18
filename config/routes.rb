d4d7c19b9fafd3012ee187cca737841ae36bfb50
paopapaopao <paolo.casugay@gmail.com>
2021-08-18 12:04:15 +0800

    Update: routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'categories#index'
+  # root to: 'categories#index'
 
   resources :categories do
     resources :tasks
