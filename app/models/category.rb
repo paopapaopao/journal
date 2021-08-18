0362e5d2f1165e6711ad92de1ed2719d3c8aedbc
paopapaopao <paolo.casugay@gmail.com>
2021-08-18 11:48:14 +0800

    Update: dscription validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   validates :title,
     presence: true, uniqueness: true
   validates :description,
-    presence: true, length: { minimum: 10, maximum: 100 }
+    presence: true, length: { in: 10..100 }
 
   has_many :tasks, dependent: :destroy
 end
