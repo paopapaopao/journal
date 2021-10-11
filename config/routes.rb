949f808a1fee8e047158003a95e0a7ad98e053de
paopapaopao <paolo.casugay@gmail.com>
2021-10-11 16:54:11 +0800

    Set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: 'categories#index'
+  # root to: 'pages#home'
+  root to: 'categories#index'
+
+  devise_for :users
 
   resources :categories do
     resources :tasks
