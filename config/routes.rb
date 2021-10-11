48bddd98f92ee3c6cadaed80281b9a1f884ef804
paopapaopao <paolo.casugay@gmail.com>
2021-10-11 22:46:16 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # root to: 'pages#home'
-  root to: 'categories#index'
-
   devise_for :users
 
-  resources :categories do
+  authenticate :user do
+    root to: 'categories#index'
+
+    resources :categories do
+      resources :tasks
+    end
     resources :tasks
   end
 end
