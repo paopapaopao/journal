0e8afd49d79b00f9733b20571d9834a2802decd0
paopapaopao <paolo.casugay@gmail.com>
2021-10-11 18:27:27 +0800

    Set task user id to current user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
   def create
     @category = Category.find(params[:category_id])
-    @task = @category.tasks.create(task_params)
+    @task = @category.tasks.new(task_params)
+    @task.user_id = current_user.id
+    @task.save
     redirect_to category_path(@category)
   end
 
