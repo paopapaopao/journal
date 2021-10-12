c1d4aabd3d6d646047624f04c86ae1892e3371a2
paopapaopao <paolo.casugay@gmail.com>
2021-10-12 11:13:36 +0800

    Add default values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.create(
+  email: 'testuser@example.com',
+  password: :password
+)
+c = Category.create(
+  title: 'Category Title',
+  description: 'Lorem ipsum dolor sit amet',
+  user_id: u.id
+)
+t = Task.create(
+  description: 'Lorem ipsum dolor sit amet',
+  due_date: DateTime.now,
+  status: :unfinished,
+  user_id: u.id,
+  category_id: c.id
+)
